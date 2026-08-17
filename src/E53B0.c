4d6be566c0f3d154d1739141a24d48dace2ca384
halkun <37340407+joshuanwalker@users.noreply.github.com>
2026-08-17 17:34:20 -0500

    Set up initial JP executable split

---
+++ src/E53B0.c
@@ -0,0 +1,241 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F4BB0);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F4C74);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F5130);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F5248);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F5378);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F5538);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F56D8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F5D04);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F5F6C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6090);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F61D8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6454);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6534);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6704);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F694C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6B38);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6BD0);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6D04);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6E7C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F6FA0);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F7134);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F73AC);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F8D64);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F8FB8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F908C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F9434);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F963C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F9A1C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800F9D44);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FA16C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FA4F8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FABD8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FADDC);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FEC6C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FEE40);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FEF58);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FF478);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FF638);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FF794);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FF8A4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_800FF98C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010014C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80100400);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80100C48);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80100CD4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80100EE4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80100FD8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80101854);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010193C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80101ED4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010220C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80102BA4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80102C74);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80102DC8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80103604);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80103914);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80103E34);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80104A64);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80104AF4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80104E48);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_801050FC);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_801051CC);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_801052B8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010666C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_801067C8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80106A84);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80106C50);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80106D44);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_801072C0);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80109064);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80109568);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_80109BF8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010A5E4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010AB9C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010AED8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010B70C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BCA0);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BD38);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BDA8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BE58);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BED4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BEEC);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BF0C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BF64);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BF88);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BFB8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BFD8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010BFF8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C054);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C084);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C09C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C124);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C168);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C1A8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C24C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C300);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C370);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C3D4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C45C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C530);
+
+void func_8010C5F4(void) {
+}
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C5FC);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C648);
+
+void func_8010C704(void) {
+}
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C70C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C768);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C7DC);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C820);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C870);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C87C);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C8A4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C8C8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C9C8);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010C9E4);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010CA44);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010CAAC);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010CB14);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010CB70);
+
+INCLUDE_ASM("asm/jp/main/nonmatchings/E53B0", func_8010CB90);
